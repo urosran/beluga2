51d34e2c77316f5bc4efad362df2c9d3dc6d0cb1
Uros Randelovic <uros.randelovic@gmail.com>
2019-12-06 01:13:15 +0000

    all works, to implement a single call to emailing service and return transcription

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,198 +1,216 @@
-import React, { Component} from 'react'
+import React, { Component } from "react";
 // import {Grid, Typography, Button} from "@material-ui/core"
-import MyEmail from './Email'
-import { renderEmail } from 'react-html-email'
-import axios from 'axios';
-let audioUrl = undefined
-let mediaRecorder;
-let audioChunks = []
-let audioBlob = undefined
+import MyEmail from "./Email";
+import { renderEmail } from "react-html-email";
+import axios from "axios";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import { Grow } from "@material-ui/core";
 
+let audioUrl = undefined;
+let mediaRecorder;
+let audioChunks = [];
+let audioBlob = undefined;
+let url = undefined;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: 200
+    }
+  }
+}));
 
 export default class CustomCard extends Component {
-    constructor(props) {
-        super(props);
-        this.record = this.record.bind(this);
-        this.startRecording = this.startRecording.bind(this);
-        this.stopRecording = this.stopRecording.bind(this);
-        this.state={}
-        this.speech_to_text=this.speech_to_text.bind(this)
-        this.submit=this.submit.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.name = ''
-        this.email = ''
-        this.handleEmailChange=this.handleEmailChange.bind(this)
-        this.handleNameChange=this.handleNameChange.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.record = this.record.bind(this);
+    this.startRecording = this.startRecording.bind(this);
+    this.stopRecording = this.stopRecording.bind(this);
+    this.state = { download_url: undefined, transcripton: "" };
+    this.speech_to_text = this.speech_to_text.bind(this);
+    this.submit = this.submit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.name = "";
+    this.email = "";
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+  // classes = useStyles();
 
-    speech_to_text = async (file) => {
-        console.log(audioUrl, "file")
-        
-        let blob = await fetch(audioUrl).then(r => r.blob());
-        console.log(blob, "blob")
-        var file = new File([audioBlob], "audio");
-        console.log(file, "file text")
+  speech_to_text = async file => {
+    console.log(audioUrl, "file");
 
-        const photoFormData = new FormData();
+    let blob = await fetch(audioUrl).then(r => r.blob());
+    console.log(blob, "blob");
+    var file = new File(
+      [audioBlob],
+      "audio.weba",
+      { type: "audio/weba" },
+      { contentType: "audio/weba" },
+      { mimetype: "audio/weba" }
+    );
+    console.log(file, "file text");
+    console.log(file.mimetype, "file mime");
 
-        photoFormData.append("avatar", file);
-        const response = await fetch(`http://localhost:5000/photo`, {
-            method: 'POST',
-            body: photoFormData,
-        });
+    const photoFormData = new FormData();
 
-        return response.text();
-    }
-    
-    record = () => {
-        navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(stream => {
+    photoFormData.append("avatar", file);
+    const response = await fetch(`http://localhost:5001/photo`, {
+      method: "POST",
+      body: photoFormData
+    });
+    audioChunks = [];
+    console.log(response.text())
+    console.log(response)
+    return response.text();
+  };
 
-            mediaRecorder = new MediaRecorder(stream);
-            console.log(mediaRecorder.state, "top state")
-            
-            audioChunks = [];
-            console.log("im inside")
+  record = () => {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+      let options = {
+        mimeType: "audio/webm"
+      };
+      mediaRecorder = new MediaRecorder(stream, options);
+      audioChunks = [];
+      console.log("im inside");
 
-            // mediaRecorder.addEventListener("dataavailable", event => {
-            //     console.log("inside data available")
-            //     audioChunks.push(event.data);
-            // });
-            mediaRecorder.ondataavailable = function(e) {
-                console.log("pushing audio")
-                audioChunks.push(e.data);
-              }
-            
-            mediaRecorder.addEventListener("stop", () => {
-                console.log('inside stop')
-                audioBlob = new Blob(audioChunks);
-                console.log(audioBlob)
-                audioUrl = URL.createObjectURL(audioBlob);
-                console.log(audioUrl, "audio")
-            console.log(mediaRecorder.state, "stop state")
+      mediaRecorder.addEventListener("dataavailable", event => {
+        console.log("inside data available");
+        audioChunks.push(event.data);
+      });
+      mediaRecorder.ondataavailable = function(e) {
+        console.log("pushing audio");
+        audioChunks.push(e.data);
+      };
 
-            });
-    })}
-
-    startRecording=()=>{
-        console.log("pressed")
-        mediaRecorder.start()
-        console.log(mediaRecorder.state, "inisde startRec")
-
-    }
-
-    stopRecording = () => {
-         mediaRecorder.stop()
-        console.log("passing in audio", audioUrl)
-    }        
-    submit= () => {
-        this.speech_to_text(audioUrl).catch(console.error)
-    }
-    componentDidMount(){
-        this.record()
-    }
-
-    // async speech_to_text(){
-    //     // Imports the Google Cloud client library
-    //     const speech = require('@google-cloud/speech');
-
-    //     // Creates a client
-    //     const client = new speech.SpeechClient(
-    //         {
-    //             type: "service_account",
-    //             project_id: "survey-1571413046810",
-    //             private_key_id: "7acd6866ada1555de93f16adc33eadc291426cf3",
-    //             private_key: "-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQCx+zqmazA7m40F\ndbMH7Jchnoor9rWVY3KUpulUbnAfwZH5eUup8FeOF1kP/nSX4yN+QOpJDnwrnKeV\n1yPJVMo1z0eZs0LT3uL7L4RXjIYRuSfVEWWBPXjteC5h4jO3THlFaNRoQkck157j\nCBrdKVbTrBHs5dai1XTVyskkowrP/y6Bls/hH8No1Ev/wc5KsZ1v4F4lTAK9NmVV\nC9ewZu8oEvPVPtwzgOvWMauMnRX4w343SA7oLrTxGV5K0uGcT9xvSIPYzq8FSkMJ\nBHkj2hpIrbA9X3xIjbv3wFBcv7Xk5PWWyOk1XcMjOHcN3BMbfRkCNPaAxQA/V+yr\nrQ2o5zGZAgMBAAECgf908V7DYw3Ykh65BPDYMHqLeud1zs6Nhn7HDA1NmXgSp0zm\nZowXVOCfql4ZkrinPenAYGYnRpaS9rUS1wH5fKI9sfchz93TGuNICUjPrBnGTHaS\nsafFe1zkmn/nwuBRVS0VAmjUD+wl73keYM7QZkLIQJstDNjt/ctP3FL4JydmS5ja\nE4YHdwOBdhjlnlNJ8/IEMrNWvSwn18c4r8AXLpJUfyL34EcQPJxJnflmSQ6uCojY\nI6exnQVBCXS503SAxzF+NocPoioc1lSuXbJbPjF7IXh0+hLLpn07VggTgaxDfAL1\nHGO5YxiJe3sVDGk3VM4Dkp0ynXlA5aBGR3aSyQkCgYEA5/QJnbPJrZCGNZoRMBQa\nuSr9XS6sc1nJoUKz+EJDMVQRhvz5iG4N+rkuCibdJHqL9xwlczlWUon1Kr8LGkCe\nvrQ+zOVUOslcZNvYqN6bN6+BAUx3Xc9RLY4RcYlyO49XnmrdbS13kL+tLBfwQ5Ii\nWdy4dXe1IZsQ0Qzd5I+fFIsCgYEAxG7LjDOIdcEpIlwtDYnHz4qCxXTXCPqOBC+T\nVmNwgpeIKqT64Kd5oCkTe4pHuPft7KJVsYsdyc4DSPbowLyGLrdZyc2s+6olUCXK\nRIA6G0NpbScQrapCRNlw0a57PBw8d6paW0YDtBN9l+C08bsMFxnNDOlZ+skFkMOY\nnuCqwusCgYEAtpZmv2pYVrVOwIEQCJ26z4okuaZU/9RtIPEGBGX8yKbZyT34G8ou\nXPBKaplfOSjyZsoUPNikuAJ7KHQMhFTb4Vi3msjnfgMmJQreSUg6PSJ9VY1g5Uqe\nJ1MPLiW482OJ4F1VAm1pElugMPaDHl25hq110poxzktneHg9b38TBbMCgYBfGEAk\nJIKIdzWhs7v/1NbNa4G5c8HyT9HWxu1uIf/CyPcbpfjqL0mbNqd+5EeqjSQ6rbAM\nnouSOdCvTxEI1XukPBEUuCRpRUqBDXppRSZcMntkY0gYutcQG7AZYn24cPb4+5h4\nnWGHmPPxRdBzQhktSclR2do8pXqzRW2qZJtmdwKBgACAJa2ei9We9Iw6F0I5I2Tm\nQalfrKB7+zQmU2XVsYNdlEKk/M99WD9SNFCpI06bJavtCjMfXIAVi+okC6tTh+km\nsj2yYzlK19tRWr/fEEsoZS8NsFLE8WcMw9TsBUTT0qYLxBh7tel7bEiR8yqq527W\nalQ1igCrDja7Xb84abx+\n-----END PRIVATE KEY-----\n",
-    //             client_email: "testacct@survey-1571413046810.iam.gserviceaccount.com",
-    //             client_id: "100986276234088870248",
-    //             auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    //             token_uri: "https://oauth2.googleapis.com/token",
-    //             auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    //             client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/testacct%40survey-1571413046810.iam.gserviceaccount.com"
-    //           }
-              
-    //     );
-
-    //     /**
-    //      * TODO(developer): Uncomment the following lines before running the sample.
-    //      */
-    //     const gcsUri = audioUrl;
-    //     const encoding = 'LINEAR16';
-    //     const sampleRateHertz = 16000;
-    //     const languageCode = 'en-US';
-
-    //     const config = {
-    //     encoding: encoding,
-    //     sampleRateHertz: sampleRateHertz,
-    //     languageCode: languageCode,
-    //     };
-
-    //     const audio = {
-    //     uri: gcsUri,
-    //     };
-
-    //     const request = {
-    //     config: config,
-    //     audio: audio,
-    //     };
-
-    //     // Detects speech in the audio file. This creates a recognition job that you
-    //     // can wait for now, or get its result later.
-    //     const [operation] = await client.longRunningRecognize(request);
-    //     // Get a Promise representation of the final result of the job
-    //     const [response] = await operation.promise();
-    //     const transcription = response.results
-    //     .map(result => result.alternatives[0].transcript)
-    //     .join('\n');
-    //     console.log(`Transcription: ${transcription}`);
-    // }
-    // main().catch(console.error);
-
-    handleSubmit(event){
-
-        const messageHtml =  renderEmail(
-            <MyEmail name={this.state.name}></MyEmail>
+      mediaRecorder.addEventListener("stop", () => {
+        console.log("inside stop");
+        audioBlob = new Blob(
+          audioChunks,
+          { type: "audio/webm" },
+          { mimetype: "audio/webm" }
         );
-            axios({
-                method: "POST", 
-                url:"http://localhost:5000/send", 
-                data: {
-                name: this.state.name,
-                email: this.state.email,
-                messageHtml: messageHtml
-                }
-            }).then((response)=>{
-                if (response.data.msg === 'success'){
-                    alert("Email sent, awesome!"); 
-                    this.resetForm()
-                }else if(response.data.msg === 'fail'){
-                    alert("Oops, something went wrong. Try again")
-            }
-        })
+        // audioBlob.type = 'audio/webm'
+        console.log(audioBlob);
+        console.log(audioBlob.type, "blob");
+        // console.log(audioBlob.mimetype, 'blob')
+        audioUrl = URL.createObjectURL(audioBlob);
+        // Play the audio
+        // const audio = new Audio(audioUrl);
+        // audio.play();
+        console.log(audioUrl, "audio");
+        this.setState(() => (url = audioUrl));
+
+        console.log(mediaRecorder.state, "stop state");
+        console.log(MediaRecorder.mimeType, "mime");
+      });
+    });
+  };
+
+  startRecording = () => {
+    console.log("pressed");
+
+    if (mediaRecorder.state != "recording") {
+      mediaRecorder.start();
+    } else {
+      alert("already recording");
     }
-    handleEmailChange(event) {
-        this.setState({email: event.target.value});
+
+    console.log(mediaRecorder.state, "inisde startRec");
+  };
+
+  stopRecording = () => {
+    if (mediaRecorder.state != "inactive") {
+      mediaRecorder.stop();
+    } else {
+      alert("Please start the recording");
     }
-    handleNameChange(event) {
-        this.setState({name: event.target.value});
-    }
-    render(){
-        return(        
-            <div>
-                <button onClick={this.startRecording}>start</button>
-                <button onClick={this.stopRecording}>stop</button>
-                <button onClick={this.submit}>submit</button>
-                <form>
-                    <label>
-                        Name:
-                        <input type="text" name="name" onChange={this.handleNameChange}/>
-                    </label>
-                    <label>
-                        Email:
-                        <input type="text" name="email" onChange={this.handleEmailChange}/>
-                    </label>
-                    <input type="submit" value="Submit" onClick={this.handleSubmit} />
-                </form>
-            </div>
-    )}
+    console.log("passing in audio", audioUrl);
+  };
+  submit = () => {
+    this.setState({
+      transcription: this.speech_to_text(audioUrl).catch(console.error)
+    });
+  };
+  componentDidMount() {
+    this.record();
+  }
+
+  handleSubmit(event) {
+    const messageHtml = renderEmail(<MyEmail name={this.state.name}></MyEmail>);
+    axios({
+      method: "POST",
+      url: "http://localhost:5001/send",
+      data: {
+        name: this.state.name,
+        email: this.state.email,
+        messageHtml: messageHtml
+      }
+    }).then(response => {
+      console.log(response.data.msg, "data");
+      if (response.data.msg === "success") {
+        alert("Email sent, awesome!");
+        this.resetForm();
+      } else if (response.data.msg === "fail") {
+        alert("Oops, something went wrong. Try again");
+      }
+    });
+  }
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+  handleNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+  render() {
+    return (
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid item style={{ paddingTop: 50, marginBottom: 100 }}>
+          <Typography variant="h4">Please provide your feedback</Typography>
+        </Grid>
+
+        <form noValidate autoComplete="off">
+          <TextField
+            id="standard-basic"
+            label="Name"
+            type="text"
+            onChange={this.handleNameChange}
+            style={{ margin: 10 }}
+          />
+          <TextField
+            id="filled-basic"
+            label="Email"
+            type="text"
+            onChange={this.handleEmailChange}
+            style={{ margin: 10 }}
+          />
+        </form>
+        <Button onClick={this.handleSubmit}>send test email</Button>
+        <Grid item>
+            <Typography>You said:</Typography>
+            {console.log(this.state.transcripton)}
+            <Typography>{this.state.transcripton}</Typography>
+        </Grid>
+        <Grid item direction="column" justify="center" alignItems="center">
+          <Button color="primary" onClick={this.startRecording}>
+            start
+          </Button>
+          <Button color="secondary" onClick={this.stopRecording}>
+            stop
+          </Button>
+          <Button onClick={this.submit}>submit</Button>
+          <Button href={audioUrl} download="test.weba">
+            Download
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
 }
--- server.js
+++ server.js
@@ -1,140 +1,157 @@
-var express = require('express')
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
-const bodyParser = require('body-parser')
+var express = require("express");
+var multer = require("multer");
+// var upload = multer({ dest: 'uploads/' })
+const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors')
-const nodemailer = require('nodemailer');
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const linear16 = require("linear16");
+const mime = require("mime");
 
-var allowedOrigins = [
-  'http://localhost:3000',
-  'http://yourapp.com'
-];
-
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-      'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
+var allowedOrigins = ["http://localhost:3000", "http://yourapp.com"];
+const STATIC_FILES_DIR = "./uploads";
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, STATIC_FILES_DIR);
+  },
+  filename(req, file = {}, cb) {
+    file.mimetype = "audio/webm";
+    const { originalname } = file;
+    const fileExtension = (originalname.match(/\.+[\S]+$/) || [])[0];
+    cb(null, `${file.fieldname}__${Date.now()}${fileExtension}`);
   }
-}));
-app.use(bodyParser.json())
-
-app.listen(5000, function(){
-  console.log("App listening on", 5000)
 });
+
+const upload = multer({ storage });
+
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
+app.use(bodyParser.json());
+
+app.listen(5001, function() {
+  console.log("App listening on", 5001);
+});
+
 // ROUTES
 
 // recieve the voice
-app.post('/photo', upload.single('avatar'), function (req, res, next) {
+app.post("/photo", upload.single("avatar"), function(req, res, next) {
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
-  console.log(req.file)
-  if (req.file == undefined){
-    res.sendStatus(400)
-  }else{
-    res.send(main(req.file.path).catch(console.error));
-  }
-})
- 
-// send emails
-app.post('/send', (req, res, next) => {
-  console.log(req.body, 'req')
-  const name = req.body.name
-  const email = req.body.email
-  const message = req.body.messageHtml
+  // console.log(req.file)
+  // console.log(mime.getType(req.file))
+  // console.log(req.file)
 
-  console.log(name, "name")
-  console.log(email, "email")
-  console.log(message, "message")
+  if (req.file == undefined) {
+    res.sendStatus(400);
+  } else {
+    res.send(googleVoiceTranscribe(req.file.path).catch(console.error));
+  }
+});
+
+// send emails
+app.post("/send", (req, res, next) => {
+  console.log(req.body, "req");
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.messageHtml;
+
+  console.log(name, "name");
+  console.log(email, "email");
+  console.log(message, "message");
 
   var mail = {
     from: name,
-    to: email,  
-    subject: 'Contact form request',
+    to: email,
+    subject: "Contact form request",
 
     html: message
-  }
+  };
 
   transporter.sendMail(mail, (err, data) => {
     if (err) {
-      console.log("fail", err)
+      console.log("fail", err);
 
       res.json({
-        msg: 'fail'
-      })
+        msg: "fail"
+      });
     } else {
-      console.log("poslato")
+      console.log("poslato");
       res.json({
-        msg: 'success'
-      })
+        msg: "success"
+      });
     }
-  })
-})
+  });
+});
 
 var transport = {
-  host: 'smtp.gmail.com', // e.g. smtp.gmail.com
+  host: "smtp.gmail.com", // e.g. smtp.gmail.com
   auth: {
-    user: 'uros.randelovic@gmail.com',
-    pass: 'npgmoeenkuzfejww'
+    user: "uros.randelovic@gmail.com",
+    pass: "npgmoeenkuzfejww"
   }
-}
+};
 
-var transporter = nodemailer.createTransport(transport)
+var transporter = nodemailer.createTransport(transport);
 
 transporter.verify((error, success) => {
   if (error) {
     console.log(error);
   } else {
-    console.log('All works fine, congratz!');
+    console.log("All works fine, congratz!");
   }
 });
 
-
-
-
-
 // google returns the recognized text from the method
-async function main(fileName) {
+async function googleVoiceTranscribe(fileName) {
   // Imports the Google Cloud client library
-  const speech = require('@google-cloud/speech');
-  const fs = require('fs');
+  const speech = require("@google-cloud/speech");
+  const fs = require("fs");
 
   // Creates a client
   const client = new speech.SpeechClient();
 
-  // The name of the audio file to transcribe
-  // const fileName = './resources/audio.raw';
-  console.log(fileName)
   // Reads a local audio file and converts it to base64
-  const file = fs.readFileSync(fileName);
-  const audioBytes = file.toString('base64');
+  console.log(mime.getType(fileName), "mime");
+  const outPath = await linear16(fileName, "./output1.wav");
+  console.log(outPath); // Returns the output path, ex: ./output.wav
+  const file = fs.readFileSync(outPath);
+  const audioBytes = file.toString("base64");
 
   // The audio file's encoding, sample rate in hertz, and BCP-47 language code
   const audio = {
-    content: audioBytes,
+    content: audioBytes
   };
   const config = {
-    encoding: 'LINEAR16',
+    encoding: "LINEAR16",
     sampleRateHertz: 16000,
-    languageCode: 'en-US',
+    languageCode: "en-US"
   };
   const request = {
     audio: audio,
-    config: config,
+    config: config
   };
 
   // Detects speech in the audio file
   const [response] = await client.recognize(request);
   const transcription = response.results
     .map(result => result.alternatives[0].transcript)
-    .join('\n');
+    .join("\n");
+  
   console.log(`Transcription: ${transcription}`);
-  return transcription
+  return transcription;
 }
